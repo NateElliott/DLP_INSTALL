c51181aec7c880a241ae5ca248ff4916f19e2051
Nate Elliott <nelliott.ca@gmail.com>
2016-11-13 15:51:08 -0400

    fixed code and stuff

--- install.sh
+++ install.sh
@@ -8,56 +8,49 @@ REPO='https://github.com/NateElliott/DLP.git'
 BRAN='dev'
 
 
-if [ "$1" = "remove" ]
-then 
-
-printf "\n${WT}Removing DLP${NC}\n"
-
-printf "... ${GR}Removing backbone${NC}\n"
-sudo rm -rf backbone/ 
-
-printf "... ${GR}Removing DLP(core)${NC}\n"
-sudo rm -rf DLP/
-
-printf "... ${GR}Cleaning up${NC}\n"
-sudo rm runserver.sh
-
-printf "\n${WT}Removal complete${NC}\n\n"
-
-else
-
 printf "\n${WT}Installing DLP${NC}\n"
 read -p "$(echo -e "Enter branch:")" BRAN
 
 printf "... ${GR}Preparing to install${NC}\n"
-sudo apt-get -qq update
+apt-get -qq update
 
 printf "... ${GR}Installing Git${NC}\n"
-sudo apt-get -qq install git
+apt-get -qq install git
 
 printf "... ${GR}Installing virtualenv${NC}\n"
-sudo apt-get -qq install python-virtualenv
+apt-get -qq install python-virtualenv
 
 printf "... ${GR}Preparing virtualenv${NC}\n"
-sudo virtualenv -q -p python3 backbone
+virtualenv -q -p python3 backbone
 
 printf "... ${GR}Installing Django${NC}\n"
-sudo backbone/bin/pip -q install Django
+backbone/bin/pip -q install Django
 
 printf "... ${GR}Cloning branch($BRAN)${NC}\n"
-sudo git clone -b $BRAN -q $REPO
+git clone -b $BRAN -q $REPO
 
 
-sudo touch runserver.sh
+touch runserver.sh
 cat <<EOT >> runserver.sh
 #!/bin/bash
-sudo backbone/bin/python DLP/playground/manage.py runserver
+backbone/bin/python DLP/playground/manage.py runserver
 EOT
 
-sudo chmod 777 runserver.sh
+touch uninstall.sh
+cat <<EOT >> uninstall.sh
+printf "... ${GR}Removing backbone${NC}\n"
+rm -rf backbone/
+printf "... ${GR}Removing DLP(core)${NC}\n"
+rm -rm DLP
+printf "... ${GR}Cleaning up...${NC}\n"
+rm uninstall.sh
+rm runserver.sh
+
+EOT
+
+chmod 777 runserver.sh
+chmod 777 uninstall.sh
 printf "${WT}Install complete!${NC}\n\n"
 
-printf "Use: ${GR}sudo ./runserver.sh${NC} to start local dev server\n\n"
-
-fi
-
+printf "Use: ${GR}sudo ./runserver.sh${NC} to start local dev server\n"
+printf "Use: ${GR}sudo ./uninstall.sh${NC} to remove local development environment\n\n"
